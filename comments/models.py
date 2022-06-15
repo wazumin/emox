fd2be15f9d7522f8274e26b71a13b901a4718082
wazimikaito <k8w824@gmail.com>
2022-06-15 20:26:17 +0900

    model and superuser are created

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Comment(models.Model):
+    emotion = models.CharField('感情', max_length=30)
+    description = models.TextField('説明', blank=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="投稿者", on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField("登校日", auto_now_add=True)
+    updated_at = models.DateTimeField("更新日", auto_now=True)
+
+    def __str__(self):
+        return self.emotion
